59ce3b2ec6de6f10fee2d4c84e449506e46599cf
Jonathan Seitel <jon.seitel@gmail.com>
2013-04-14 18:26:49 -0700

    Adding functioning view/routing layer

--- core/views.py
+++ core/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.template import Context, loader
 
 def index(request):
-    return HttpResponse("Hello, world")
+    template = loader.get_template('index.html')
+    context = Context({
+        'test_var': '8=====D', 
+    })
+    return HttpResponse(template.render(context))
