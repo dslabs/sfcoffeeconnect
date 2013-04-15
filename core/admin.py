4c59efd63085a6d23a1a8b21e3b90e1e94bf0eda
Mike Repass <mike.repass@gmail.com>
2013-04-14 17:43:31 -0700

    a basic CoffeeMatch model now exists with admin...

---
+++ core/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from core.models import CoffeeMatch
+
+
+class CoffeeMatchAdmin(admin.ModelAdmin):
+    readonly_fields = ["created", "updated"]
+    list_display = ["recipient", "candidate", "accepted", "updated"]
+    list_filter = ["accepted", "updated", "recipient", "candidate"]
+    search_fields = ["recipient", "candidate"]
+    fieldsets = [
+        ("Meta", {"fields": ["created", "updated"]}),
+        ("People", {"fields": ["recipient", "candidate"]}),
+        ("Status", {"fields": ["accepted"]}),
+    ]
+
+admin.site.register(CoffeeMatch, CoffeeMatchAdmin)
--- core/models.py
+++ core/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class CoffeeMatch(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    recipient = models.ForeignKey(User, related_name='recipient')
+    candidate = models.ForeignKey(User, related_name='candidate')
+    accepted = models.BooleanField(default=False)
+
+    class Meta:
+        verbose_name_plural = "Coffee Matches"
