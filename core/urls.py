7ef32eebf47f31a911f9fc1911667a572a25f527
Jonathan Seitel <jon.seitel@gmail.com>
2013-04-14 18:17:51 -0700

    some views shite

--- core/urls.py
+++ core/urls.py
@@ -1,13 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-from django.contrib import admin
-admin.autodiscover()
-
 urlpatterns = patterns('',
-    url(r'^core/', include('core.urls')),
+    url(r'^$', 'core.views.index'),
     # Examples:
     # url(r'^$', 'sfcoffeeconnect.views.home', name='home'),
     # url(r'^sfcoffeeconnect/', include('sfcoffeeconnect.foo.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
 )
